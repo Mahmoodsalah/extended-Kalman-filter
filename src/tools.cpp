2eb43c4ef4c33b96c7fdd082bec3d3f855e5e7bc
Mahmood Salah <mahmoodsalah89@gmail.com>
2017-04-16 22:45:05 +0300

    Fix: resolve zero problems with second input data file

--- src/tools.cpp
+++ src/tools.cpp
@@ -57,34 +57,27 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
  *******************************************************************************/
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 
-    MatrixXd Hj(3,4);
-    //recover state parameters
     double px = x_state(0);
     double py = x_state(1);
     double vx = x_state(2);
     double vy = x_state(3);
-
-    //
-
-    //accelartor variables
-    double c1 = px*px + py*py;
-    double c2 = sqrt(c1);
-    double c3 = c1*c2;
-
-
-    //check division by zero
-    if (fabs(c1) < 0.00001){
-        VectorXd x_stemp(4);
-        // in case of division by zero, set state to small float value
-        x_stemp << 0.001, 0.01, 0.001, 0.001;
-        std::cout<<"Error - Division by Zero (px and py equal zero)"<< std::endl;
-        return CalculateJacobian(x_stemp);
+    MatrixXd Hj = MatrixXd::Zero(3,4);
+    double dist = sqrt(pow(px,2) + pow(py,2));
+    double dist_2 = dist*dist;
+    double dist_3 = dist_2*dist;
+    double cross_mult = vx*py - vy*px;
+    double px_dist = px/dist;
+    double py_dist = py/dist;
+    float tol = 0.0001;
+    if (dist > tol){
+        Hj(0,0) = px_dist;
+        Hj(0,1) = py_dist;
+        Hj(1,0) = -py/dist_2;
+        Hj(1,1) = px/dist_2;
+        Hj(2,0) = py*cross_mult/dist_3;
+        Hj(2,1) = -px*cross_mult/dist_3;
+        Hj(2,2) = px_dist;
+        Hj(2,3) = py_dist;
     }
-    //compute the Jacobian matrix
-    Hj << (px/c2), (py/c2), 0, 0,
-            -(py/c1), (px/c1), 0, 0,
-            py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
-
     return Hj;
-
 }
